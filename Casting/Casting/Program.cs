8739590548312648d0ad6c6dda682677609cf164
satk66 <63436936+satk66@users.noreply.github.com>
2020-08-14 13:16:53 +0900

    Add files via upload

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    class Program
+    {
+        class Mamal
+        {
+            public Mamal()
+            {
+                Console.WriteLine("Hello world");
+            }
+            public void Nurse()
+            {
+                Console.WriteLine("동물을 길러요");
+            }
+        }
+        class Dog : Mamal
+        {
+            public Dog()
+            {
+                Console.WriteLine("Hello Dog");
+            }
+            public void Bark()
+            {
+                Console.WriteLine("멍멍");
+            }
+            class Cat : Mamal
+            {
+                public Cat()
+                {
+                    Console.WriteLine("Hello Cat");
+                }
+                public void Bark()
+                {
+                    Console.WriteLine("냐옹");
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Mamal m = new Mamal();
+                Dog d = new Dog();
+                m.Nurse();
+                d.Nurse(); d.Bark();
+
+                Mamal mc = new Cat();
+                mc.Nurse();
+                
+                
+
+                Console.Read();
+            }
+        }
+    }
+}
