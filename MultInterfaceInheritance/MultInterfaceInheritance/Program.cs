d69abd50da976b15fb04cf9fd42892afcf6cb84e
satk66 <63436936+satk66@users.noreply.github.com>
2020-08-21 18:44:10 +0900

    Add files via upload

---
+++ MultInterfaceInheritance/MultInterfaceInheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MultInterfaceInheritance
+{
+    interface IRunnable
+    {
+        void Run();
+    }
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    class FlyingCat : IRunnable, IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Run! Run!");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Fly! Fly!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlyingCat car = new FlyingCat();
+            car.Run();
+            car.Fly();
+
+            IRunnable runnable = car as IRunnable;
+            runnable.Run();
+
+            IFlyable flyable = car as IFlyable;
+            flyable.Fly();
+
+        }
+    }
+}
