f6c9fe44a13d5b5e6b1321254aae1654c84e2405
satk66 <63436936+satk66@users.noreply.github.com>
2020-08-24 17:34:05 +0900

    Add files via upload

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    abstract class AbstractBase
+        {
+    protected void PrivateMethoda()
+    {
+        Console.WriteLine("AbstravtBase.PrivateMethoda()");
+    }
+        public void PrivateMethoda()
+    {
+       Console.WriteLine("AbstravtBase.PrivateMethoda()");
+    }
+        public abstract void AbstractMethodA();
+   }
+class Derived : AbstractBase
+{
+    // public void PrivateMethodA()
+    // {
+    //    Console.WriteLine(" PrivateMethodA()")
+    //}
+
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Derived.AbstractMethodA()");
+            PrivateMethoda();
+        }
+    }
+class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractBase obj = new Derived();
+            obj.AbstractMethodA();
+            obj.PrivateMethoda();
+        }
+    }
+}
