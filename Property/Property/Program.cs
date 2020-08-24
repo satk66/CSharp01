00717576e5674113295c6494223b84c8055c92b6
satk66 <63436936+satk66@users.noreply.github.com>
2020-08-24 17:47:12 +0900

    Add files via upload

---
+++ Property/Property/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Property
+{
+    class BirthdayInfo
+    {
+        private string name;
+        private DateTime birthday;
+
+        public string Name
+        {
+         get {   //읽기 전용
+                return name;
+                 
+            }
+            set
+            { //쓰기전용
+                name = value;
+            }
+        }
+        public DateTime Birthday
+        {
+            get
+            {
+                return birthday;
+            }
+            set
+            {
+                birthday = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(birthday).Ticks).Year;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            birth.Name = "마마"; //Name 의 set
+            birth.Birthday = new DateTime(2000, 10, 19); //set
+            Console.WriteLine(birth.Name); //Name 의 get
+            Console.WriteLine(birth.Birthday); //Birthday 의 get
+            Console.WriteLine(birth.Age); // Age의 get
+        }
+    }
+}
