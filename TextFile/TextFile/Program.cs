b54223175218beb3358a93ae7e086e25dc89af0f
satk66 <63436936+satk66@users.noreply.github.com>
2020-09-07 04:40:39 +0900

    Add files via upload

---
+++ TextFile/TextFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace TextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(new FileStream("a.txt", FileMode.Create));
+            sw.Write(int.MaxValue);
+            sw.Write("Good Morning");
+            sw.Write(uint.MaxValue);
+            sw.Write("안녕하세여");
+            sw.Write(double.MaxValue);
+            sw.Close();
+
+            StreamReader sr = new StreamReader(new FileStream("a.txt", FileMode.Open));
+            Console.WriteLine($"File size : {sr.BaseStream.Length} bytes");
+            while (sr.EndOfStream == false) 
+                Console.WriteLine(sr.ReadLine());
+
+            sr.Close();
+        }
+    }
+}
