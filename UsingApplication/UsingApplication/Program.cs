6d725fa82c0e212fffba6461cf319c66e3a8226b
satk66 <63436936+satk66@users.noreply.github.com>
2020-09-09 16:04:54 +0900

    Add files via upload

---
+++ UsingApplication/UsingApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingApplication
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Program form = new Program();
+            form.Click += new EventHandler(
+                (sender,eventArgs)=> {
+                    Console.WriteLine("Closing window");
+                    Application.Exit();
+                });
+            Console.WriteLine("Starting");
+            Application.Run(form);
+            Console.WriteLine("Exiting");
+        }
+    }
+}
