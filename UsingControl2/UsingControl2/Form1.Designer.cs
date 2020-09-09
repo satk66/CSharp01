829a73943df2c3b6d8289e5fccd9e730c1f78419
satk66 <63436936+satk66@users.noreply.github.com>
2020-09-09 17:48:39 +0900

    Add files via upload

---
+++ UsingControl2/UsingControl2/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace UsingControl2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpFont;
+        private System.Windows.Forms.Label IbFont;
+        private System.Windows.Forms.TextBox txtSampleText;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox chkBold;
+        private System.Windows.Forms.ComboBox cboFont;
+        private System.Windows.Forms.GroupBox grpBar;
+        private System.Windows.Forms.TrackBar tbDummy;
+        private System.Windows.Forms.ProgressBar pgDummy;
+        private System.Windows.Forms.GroupBox GrpForm;
+        private System.Windows.Forms.Button btnMsgBox;
+        private System.Windows.Forms.Button btnModaless;
+        private System.Windows.Forms.Button bthModal;
+        private System.Windows.Forms.GroupBox grpTreeList;
+        private System.Windows.Forms.TreeView lvDummy;
+        private System.Windows.Forms.TreeView tvDummy;
+        private System.Windows.Forms.Button bthAddRoot;
+        private System.Windows.Forms.Button bthAddChild;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
